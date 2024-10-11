86d9ab2eb22af11e3126303c88870013bcfe7fbf
lcf2350 <c0921819@mylambton.ca>
2024-10-11 14:06:36 -0400

    Updates with images

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # MongoDB Atlas connection
-client = MongoClient("your_mongodb_atlas_connection_string")
+client = MongoClient("mongodb+srv://c0921819:$itaKrishna7@ecommerce-cluster.0xwr1.mongodb.net/?retryWrites=true&w=majority")
 db = client['shop_db']
 products_collection = db['products']
 
